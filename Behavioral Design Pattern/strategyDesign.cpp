ae9405660f4711fcbdaaf7b1f3f8c207b9c35e24
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2026-02-22 00:27:53 +0530

    strategy design pattern done

--- Behavioral Design Pattern/strategyDesign.cpp
+++ Behavioral Design Pattern/strategyDesign.cpp
@@ -1,58 +1,135 @@
 #include <iostream>
-#include <memory>
+using namespace std;
 
-// 1. The Strategy Interface: Defines the "Contract"
-class IPaymentStrategy {
+// --- Strategy Interface for Walk ---
+class WalkableRobot {
 public:
-    virtual ~IPaymentStrategy() {}
-    virtual void collectPayment(double amount) = 0;
+    virtual void walk() = 0;
+    virtual ~WalkableRobot() {}
 };
 
-// 2. Concrete Strategy A: Credit Card
-class CreditCardStrategy : public IPaymentStrategy {
+// --- Concrete Strategies for walk ---
+class NormalWalk : public WalkableRobot {
 public:
-    void collectPayment(double amount) override {
-        std::cout << "Paid $" << amount << " using Credit Card (2% fee).\n";
+    void walk() override { 
+        cout << "Walking normally..." << endl; 
     }
 };
 
-// 3. Concrete Strategy B: Bitcoin
-class BitcoinStrategy : public IPaymentStrategy {
+class NoWalk : public WalkableRobot {
 public:
-    void collectPayment(double amount) override {
-        std::cout << "Paid $" << amount << " using Bitcoin (Network fee applies).\n";
+    void walk() override { 
+        cout << "Cannot walk." << endl; 
     }
 };
 
-// 4. The Context: The object that uses the strategy
-class ShoppingCart {
-private:
-    std::unique_ptr<IPaymentStrategy> strategy; // Composition
-public:
-    // We can change the strategy at any time (Runtime)
-    void setPaymentMethod(std::unique_ptr<IPaymentStrategy> s) {
-        strategy = std::move(s);
-    }
 
-    void checkout(double total) {
-        if (strategy) {
-            strategy->collectPayment(total);
-        } else {
-            std::cout << "Please select a payment method!\n";
-        }
+// --- Strategy Interface for Talk ---
+class TalkableRobot {
+public:
+    virtual void talk() = 0;
+    virtual ~TalkableRobot() {}
+};
+
+// --- Concrete Strategies for Talk ---
+class NormalTalk : public TalkableRobot {
+public:
+    void talk() override { 
+        cout << "Talking normally..." << endl; 
     }
 };
 
+class NoTalk : public TalkableRobot {
+public:
+    void talk() override { 
+        cout << "Cannot talk." << endl; 
+    }
+};
+
+// --- Strategy Interface for Fly ---
+class FlyableRobot {
+public:
+    virtual void fly() = 0;
+    virtual ~FlyableRobot() {}
+};
+
+class NormalFly : public FlyableRobot {
+public:
+    void fly() override { 
+        cout << "Flying normally..." << endl; 
+    }
+};
+
+class NoFly : public FlyableRobot {
+public:
+    void fly() override { 
+        cout << "Cannot fly." << endl; 
+    }
+};
+
+// --- Robot Base Class ---
+class Robot {
+protected:
+    WalkableRobot* walkBehavior;
+    TalkableRobot* talkBehavior;
+    FlyableRobot* flyBehavior;
+
+public:
+    Robot(WalkableRobot* w, TalkableRobot* t, FlyableRobot* f) {
+        this->walkBehavior = w;
+        this->talkBehavior = t;
+        this->flyBehavior = f;
+    }
+       
+    void walk() { 
+        walkBehavior->walk(); 
+    }
+    void talk() { 
+        talkBehavior->talk(); 
+    }
+    void fly() { 
+        flyBehavior->fly(); 
+    }
+
+    virtual void projection() = 0; // Abstract method for subclasses
+};
+
+// --- Concrete Robot Types ---
+class CompanionRobot : public Robot {
+public:
+    CompanionRobot(WalkableRobot* w, TalkableRobot* t, FlyableRobot* f)
+        : Robot(w, t, f) {}
+
+    void projection() override {
+        cout << "Displaying friendly companion features..." << endl;
+    }
+};
+
+class WorkerRobot : public Robot {
+public:
+    WorkerRobot(WalkableRobot* w, TalkableRobot* t, FlyableRobot* f)
+        : Robot(w, t, f) {}
+
+    void projection() override {
+        cout << "Displaying worker efficiency stats..." << endl;
+    }
+};
+
+// --- Main Function ---
 int main() {
-    ShoppingCart cart;
+    Robot *robot1 = new CompanionRobot(new NormalWalk(), new NormalTalk(), new NoFly());
+    robot1->walk();
+    robot1->talk();
+    robot1->fly();
+    robot1->projection();
 
-    // User chooses Credit Card
-    cart.setPaymentMethod(std::make_unique<CreditCardStrategy>());
-    cart.checkout(100.0);
+    cout << "--------------------" << endl;
 
-    // User changes mind and wants to use Bitcoin
-    cart.setPaymentMethod(std::make_unique<BitcoinStrategy>());
-    cart.checkout(100.0);
+    Robot *robot2 = new WorkerRobot(new NoWalk(), new NoTalk(), new NormalFly());
+    robot2->walk();
+    robot2->talk();
+    robot2->fly();
+    robot2->projection();
 
     return 0;
 }
